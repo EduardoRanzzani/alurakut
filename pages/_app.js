6665e39b8a984ece20b8bbdd2eb96204ff3953f1
Eduardo Leite Ranzzani <eduranzzani@gmail.com>
2021-07-13 11:21:55 -0400

    Início do alurakut

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,35 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { AlurakutStyles } from '../src/lib/AlurakutCommons'
 
 const GlobalStyle = createGlobalStyle`
+/* Reset CSS*/
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: sans-serifa;
+    background-color: #d9e6f6;
   }
+
+  #__next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+
+  ${AlurakutStyles}
 `
 
 const theme = {
